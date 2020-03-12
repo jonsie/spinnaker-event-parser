a71fe76c73773d2bfb79f5584f641e4c8ab8502e
chris smalley <csmalley@netflix.com>
2020-03-12 15:45:07 -0700

    move it to the top

--- .github/workflows/release_info.sh
+++ .github/workflows/release_info.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -x
 
+# https://github.com/fsaintjacques/semver-tool/blob/master/src/semver#L5-L14
+NAT='0|[1-9][0-9]*'
+ALPHANUM='[0-9]*[A-Za-z-][0-9A-Za-z-]*'
+IDENT="$NAT|$ALPHANUM"
+FIELD='[0-9A-Za-z-]+'
+SEMVER_REGEX="\
+^[vV]?\
+($NAT)\\.($NAT)\\.($NAT)\
+(\\-(${IDENT})(\\.(${IDENT}))*)?\
+(\\+${FIELD}(\\.${FIELD})*)?$"
+
+export IS_CANDIDATE=`[[ $NEW_TAG =~ $SEMVER_REGEX && ! -z ${BASH_REMATCH[4]} ]] && echo "true" || echo "false"`
+
 # Only look to the latest release to determine the previous tag -- this allows us to skip unsupported tag formats (like `version-1.0.0`)
 export PREVIOUS_TAG=`curl --silent "https://api.github.com/repos/jonsie/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
 echo "PREVIOUS_TAG=$PREVIOUS_TAG"
@@ -16,15 +29,6 @@ CHANGELOG="${CHANGELOG//$'\r'/%0D}"
 # If the previous release tag is the same as this tag the user likely cut a release (and in the process created a tag), which means we can skip the need to create a release
 export SKIP_RELEASE=`[[ "$PREVIOUS_TAG" = "$NEW_TAG" ]] && echo "true" || echo "false"`
 
-# https://github.com/fsaintjacques/semver-tool/blob/master/src/semver#L5-L14
-NAT='0|[1-9][0-9]*'
-ALPHANUM='[0-9]*[A-Za-z-][0-9A-Za-z-]*'
-IDENT="$NAT|$ALPHANUM"
-FIELD='[0-9A-Za-z-]+'
-SEMVER_REGEX="\
-^[vV]?\
-($NAT)\\.($NAT)\\.($NAT)\
-(\\-(${IDENT})(\\.(${IDENT}))*)?\
-(\\+${FIELD}(\\.${FIELD})*)?$"
 
-export IS_CANDIDATE=`[[ $NEW_TAG =~ $SEMVER_REGEX && ! -z ${BASH_REMATCH[4]} ]] && echo "true" || echo "false"`
+
+
